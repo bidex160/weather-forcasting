a2bcce3f2808e2ed2b3168789123c7b98295cb61
Tajudeen Wariz <wtajudeen@sysserve.com>
2024-02-27 12:26:28 +0100

    Auto-generated commit

--- src/app/components/components.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { ChartComponent } from './chart/chart.component';
-
-@NgModule({
-  declarations: [ChartComponent],
-  exports: [ChartComponent],
-  imports: [],
-})
-export class ComponentsModule {}
--- src/app/components/index.model.ts
+++
@@ -1,8 +0,0 @@
-export type IChartType = 'line';
-
-export interface IChart {
-  label: string;
-  chartType: IChartType;
-  data: [];
-  labels: [];
-}
